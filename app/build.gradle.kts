1388954c84b02328a0d075b8c70b166bdd26f400
Jihyeok Seo <ji@hyeok.org>
2025-11-23 21:45:55 +0900

    Bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "ng.commu"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
